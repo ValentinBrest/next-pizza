518150cd3356946562e2f720a1a02470f3995e88
Valentin Konopatsky <fixator03@yandex.ru>
2024-12-17 13:40:07 +0300

    add global style and Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return (
-    <h1>Home Page</h1>
-  );
+    return (
+        <>
+            <h1>Home Page</h1>
+            <Button>Корзина</Button>
+        </>
+    );
 }
